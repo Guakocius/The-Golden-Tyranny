83f643b5959ba9547379bbc38d80e9f5137052a1
Guakocius <engelhardt.alexander@mein.gmx>
2025-04-29 03:19:02 +0200

    Updated newGame to use playerChoice in order to goto classSelection

--- src/core/game.cpp
+++ src/core/game.cpp
@@ -77,11 +77,8 @@ using PauseMenu = GameMenu::PauseMenu;
         std::cout << "Press Enter to continue..." << std::endl;
         std::cin.ignore();
 
-        while (std::cin.peek() != '\n') {
-            std::this_thread::sleep_for(std::chrono::milliseconds(100));
-            std::cin.ignore();
-        }
-        classSelection();
+        CurrentMenu currMenu = GameMenu::CurrentMenu::CLASS_SELECTION;
+        playerChoice(currMenu);
     }
 
     void GameMenu::classSelection() {
