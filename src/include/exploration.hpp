90261c135febb5673f78d6e8ea8ca68aa5a8c8f0
Guakocius <engelhardt.alexander@mein.gmx>
2025-05-16 13:35:04 +0200

    Added rooms Object in locations.json and refactored codebase: .h->.hpp

--- src/include/exploration.hpp
+++ src/include/exploration.hpp
@@ -1,3 +1,16 @@
 #ifndef EXPLORATION_HPP
 
+#include "game.hpp"
+
+class Exploration {
+    public:
+
+    Exploration(Player player, std::map<std::string, )
+
+    private:
+    
+    Player player;
+    std::map<std::string,
+};
+
 #endif // !EXPLORATION_HPP
